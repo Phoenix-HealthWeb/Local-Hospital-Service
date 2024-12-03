efc0b0464b6f05e97be8c7169ff7406e9cdf89d3
roveroniandrea <roveroniandrea@gmail.com>
2024-12-03 17:59:59 +0100

    mix phx.gen.auth to init auth system

--- local_hospital_service/lib/local_hospital_service_web/router.ex
+++ local_hospital_service/lib/local_hospital_service_web/router.ex
@@ -1,6 +1,8 @@
 defmodule LocalHospitalServiceWeb.Router do
   use LocalHospitalServiceWeb, :router
 
+  import LocalHospitalServiceWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule LocalHospitalServiceWeb.Router do
     plug :put_root_layout, html: {LocalHospitalServiceWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -39,6 +42,44 @@ defmodule LocalHospitalServiceWeb.Router do
 
       live_dashboard "/dashboard", metrics: LocalHospitalServiceWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", LocalHospitalServiceWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{LocalHospitalServiceWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", LocalHospitalServiceWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{LocalHospitalServiceWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", LocalHospitalServiceWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{LocalHospitalServiceWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
