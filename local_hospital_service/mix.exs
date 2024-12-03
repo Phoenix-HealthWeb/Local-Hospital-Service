1a633d04b69a3b8a92419b36955cdf32eaf3f345
roveroniandrea <roveroniandrea@gmail.com>
2024-12-03 18:22:20 +0100

    added missing dependency pbkdf2_elixir

--- local_hospital_service/mix.exs
+++ local_hospital_service/mix.exs
@@ -59,7 +59,8 @@ defmodule LocalHospitalService.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
