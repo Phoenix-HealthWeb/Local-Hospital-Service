92894bb18fc446b4ddcae6f6a921ba6d766c3def
roveroniandrea <roveroniandrea@gmail.com>
2024-12-19 12:19:41 +0100

    removed unused routes and live

--- local_hospital_service/lib/local_hospital_service_web/router.ex
+++ local_hospital_service/lib/local_hospital_service_web/router.ex
@@ -52,35 +52,16 @@ defmodule LocalHospitalServiceWeb.Router do
 
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{LocalHospitalServiceWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
       live "/users/log_in", UserLoginLive, :new
       live "/users/log_in/:token", UserLoginMagicLinkLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
     end
 
     post "/users/log_in", UserSessionController, :create
   end
 
   scope "/", LocalHospitalServiceWeb do
-    pipe_through [:browser, :require_authenticated_user]
-
-    live_session :require_authenticated_user,
-      on_mount: [{LocalHospitalServiceWeb.UserAuth, :ensure_authenticated}] do
-      live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
-    end
-  end
-
-  scope "/", LocalHospitalServiceWeb do
     pipe_through [:browser]
 
     delete "/users/log_out", UserSessionController, :delete
-
-    live_session :current_user,
-      on_mount: [{LocalHospitalServiceWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
-    end
   end
 end
