016a486168d22bb5ee5a25dc701af1bbc3f38aa2
roveroniandrea <roveroniandrea@gmail.com>
2024-12-22 13:23:53 +0100

    refactor rabbit connection params

--- local_hospital_service/config/runtime.exs
+++ local_hospital_service/config/runtime.exs
@@ -23,6 +23,9 @@ end
 # Configure the url for the NDB Rest APIs
 config :local_hospital_service, LocalHospitalService.Api, ndb_url: System.fetch_env!("NDB_URL")
 
+# Configure the url for the RabbitMQ container
+config :local_hospital_service, LocalHospitalService.NdbSyncronization, rabbit_url: System.fetch_env!("RABBITMQ_URL")
+
 if config_env() == :prod do
   database_path =
     System.get_env("DATABASE_PATH") ||
