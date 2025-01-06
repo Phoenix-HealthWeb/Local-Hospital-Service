12940fe92315898c49b68adb66a8ad72aba8bf45
retrolimber2024 <908093@stud.unive.it>
2025-01-06 15:57:49 +0100

    Added "empty cf field" error

--- local_hospital_service/lib/local_hospital_service_web/live/cf_verification_live/cf_verification_live.ex
+++ local_hospital_service/lib/local_hospital_service_web/live/cf_verification_live/cf_verification_live.ex
@@ -8,48 +8,53 @@ defmodule LocalHospitalServiceWeb.CfVerificationLive do
       socket
       |> assign(:cf_input, "")
       |> assign(:show_patient_not_found_dialog, false)
-
+      |> assign(:cf_error, nil)
     {:ok, socket}
   end
 
-  # RENDER: carichiamo la funzione "index" generata da CfVerificationHTML
   def render(assigns) do
     LocalHospitalServiceWeb.CfVerificationHTML.index(assigns)
   end
 
-  # EVENTO: check_cf
+  # Check_cf
   def handle_event("check_cf", %{"form" => %{"cf" => cf}}, socket) do
-    case Api.get("patients/#{cf}") do
-      {:ok, _data} ->
-        # CF trovato
-        {:noreply, push_redirect(socket, to: ~p"/encounters/new?patient_cf=#{cf}")}
+    if String.trim(cf) == "" do
+      # CF not filled
+      {:noreply,
+       socket
+       |> assign(:cf_error, "can't be blank!")
+       |> assign(:cf_input, "")                 # clear field
+       |> assign(:show_patient_not_found_dialog, false)}
+    else
+      case Api.get("patients/#{cf}") do
+        {:ok, _data} ->
+          {:noreply, push_redirect(socket, to: ~p"/nurses/encounters/new")}
 
-      {:error, %{status_code: 404}} ->
-        # CF non trovato
-        {:noreply,
-         socket
-         |> assign(:cf_input, cf)
-         |> assign(:show_patient_not_found_dialog, true)}
+        {:error, %{status_code: 404}} ->
+          {:noreply,
+           socket
+           |> assign(:cf_error, nil)
+           |> assign(:cf_input, cf)
+           |> assign(:show_patient_not_found_dialog, true)}
 
-      {:error, reason} ->
-        # Altri errori
-        {:noreply,
-         socket
-         |> put_flash(:error, "Error: #{inspect(reason)}")
-         |> assign(:cf_input, cf)
-         |> assign(:show_patient_not_found_dialog, false)}
+        {:error, reason} ->
+          {:noreply,
+           socket
+           |> assign(:cf_error, "Error: #{inspect(reason)}")
+           |> assign(:cf_input, cf)
+           |> assign(:show_patient_not_found_dialog, false)}
+      end
     end
   end
 
-  # EVENTO: create_patient
+  # Create_patient
   def handle_event("create_patient", _params, socket) do
     cf = socket.assigns.cf_input
-    {:noreply, push_redirect(socket, to: ~p"/admin/wards")}
+    {:noreply, redirect(socket, external: "http://localhost:4000/patients/new")}
   end
 
-  # EVENTO: cancel
+  # Cancel
   def handle_event("cancel", _params, socket) do
-    # Reset allo stato iniziale
     {:noreply,
      socket
      |> assign(:show_patient_not_found_dialog, false)
