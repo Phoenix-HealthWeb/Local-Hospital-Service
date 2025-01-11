d2087caae8823aff453633274eea174263d296cc
retrolimber2024 <908093@stud.unive.it>
2025-01-11 23:25:20 +0100

    Fixed CF fetch

--- local_hospital_service/lib/local_hospital_service_web/live/cf_verification_live/cf_verification_live.ex
+++ local_hospital_service/lib/local_hospital_service_web/live/cf_verification_live/cf_verification_live.ex
@@ -26,23 +26,16 @@ defmodule LocalHospitalServiceWeb.CfVerificationLive do
        |> assign(:cf_input, "")                 # clear field
        |> assign(:show_patient_not_found_dialog, false)}
     else
-      case Api.get("patients/#{cf}") do
-        {:ok, _data} ->
-          {:noreply, push_redirect(socket, to: ~p"/nurses/encounters/new")}
-
-        {:error, %{status_code: 404}} ->
+      case LocalHospitalService.Api.Patient.get_by_cf(cf) do
+        nil ->
           {:noreply,
            socket
            |> assign(:cf_error, nil)
            |> assign(:cf_input, cf)
            |> assign(:show_patient_not_found_dialog, true)}
+        _ ->
+          {:noreply, push_navigate(socket, to: ~p"/nurses/encounters/new")}
 
-        {:error, reason} ->
-          {:noreply,
-           socket
-           |> assign(:cf_error, "Error: #{inspect(reason)}")
-           |> assign(:cf_input, cf)
-           |> assign(:show_patient_not_found_dialog, false)}
       end
     end
   end
